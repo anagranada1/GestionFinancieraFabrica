afdc6a8637efefdca969cc26b640e0814aa8aabb
Miguel Gallego <miguel.gallego2@udea.edu.co>
2026-03-18 13:18:49 -0500

    first commit

---
+++ src/test/java/com/finanzas/gestion_financiera/GestionFinancieraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanzas.gestion_financiera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFinancieraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
