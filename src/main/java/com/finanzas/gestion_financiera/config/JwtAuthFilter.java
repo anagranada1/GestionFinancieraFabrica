e0f57ecf1375c635f128eba83f02030c5160546b
leniaso <alejandro.naranjoc@udea.edu.co>
2026-03-23 18:00:33 -0500

    cambio en jwtauthfilter, esta comentado

--- src/main/java/com/finanzas/gestion_financiera/config/JwtAuthFilter.java
+++ src/main/java/com/finanzas/gestion_financiera/config/JwtAuthFilter.java
@@ -48,7 +48,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         userRepository.findByEmail(email).ifPresent(user -> {
             UserDetails userDetails = User.withUsername(user.getEmail())
-                    .password(user.getContrasena())
+                    .password("") // No se necesita la contraseña aquí
                     .authorities(List.of())
                     .build();
 
